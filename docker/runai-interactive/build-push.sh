3ba2e3f46a672f386eb195a914a1b495e24b813f
Juraj Mičko <micko.juraj64@gmail.com>
2024-02-27 09:50:17 +0100

    runai docker images

---
+++ docker/runai-interactive/build-push.sh
@@ -0,0 +1,2 @@
+docker build --platform linux/amd64 -t jjurm/runai-interactive .
+docker push jjurm/runai-interactive
---
+++ docker/runai-job/build-push.sh
@@ -0,0 +1,2 @@
+docker build --platform linux/amd64 -t jjurm/runai-job .
+docker push jjurm/runai-job
