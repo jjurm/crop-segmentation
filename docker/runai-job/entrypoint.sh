7ea81769d77c33722b5b0c3e34ac36da3baf5d34
Juraj Mičko <micko.juraj64@gmail.com>
2024-03-08 22:19:27 +0100

    docker entrypoint: print node info

--- docker/runai-job/entrypoint.sh
+++ docker/runai-job/entrypoint.sh
@@ -1,4 +1,10 @@
 #/bin/bash
 set -Eeo pipefail
 . $HOME/.profile
+# Print NODE_NAME if set
+if [ -n "$NODE_NAME" ]; then
+  echo "NODE_NAME: $NODE_NAME"
+fi
+echo "Working directory: $(pwd)"
+echo "Running command: $@"
 exec "$@"
