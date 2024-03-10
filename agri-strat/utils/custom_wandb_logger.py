cb7b35dc840cd9d11c3b26df3909a87d0c245e97
Juraj Mičko <micko.juraj64@gmail.com>
2024-03-10 23:29:55 +0100

    experiment: fix cleaning up model artifacts
    use artifact.wait() on model checkpoint artifacts

--- agri-strat/utils/custom_wandb_logger.py
+++ agri-strat/utils/custom_wandb_logger.py
@@ -1,10 +1,13 @@
 """
 Like WandbLogger, but also cleans up model artifacts without an alias.
 """
+from pathlib import Path
 
 import wandb
 from lightning.pytorch.callbacks import ModelCheckpoint
 from lightning.pytorch.loggers import WandbLogger
+from lightning.pytorch.loggers.utilities import _scan_checkpoints
+from torch import Tensor
 
 
 class CustomWandbLogger(WandbLogger):
@@ -13,7 +16,7 @@ class CustomWandbLogger(WandbLogger):
         super().__init__(**kwargs)
         assert self._checkpoint_name is not None, "The checkpoint name must be set."
 
-        self._custom_wandb_api = wandb.Api(overrides={"project": self.experiment.project})
+        self._custom_wandb_api = wandb.Api()
 
     def after_save_checkpoint(self, checkpoint_callback: ModelCheckpoint) -> None:
         super().after_save_checkpoint(checkpoint_callback)
@@ -24,3 +27,43 @@ class CustomWandbLogger(WandbLogger):
             # Clean up all versions that don't have an alias such as 'latest'.
             if len(version.aliases) == 0:
                 version.delete()
+
+    def _scan_and_log_checkpoints(self, checkpoint_callback: ModelCheckpoint) -> None:
+        """
+        This function is copied from the superclass and modified
+        """
+
+        # get checkpoints to be saved with associated score
+        checkpoints = _scan_checkpoints(checkpoint_callback, self._logged_model_time)
+
+        # log iteratively all new checkpoints
+        for t, p, s, tag in checkpoints:
+            metadata = {
+                "score": s.item() if isinstance(s, Tensor) else s,
+                "original_filename": Path(p).name,
+                checkpoint_callback.__class__.__name__: {
+                    k: getattr(checkpoint_callback, k)
+                    for k in [
+                        "monitor",
+                        "mode",
+                        "save_last",
+                        "save_top_k",
+                        "save_weights_only",
+                        "_every_n_train_steps",
+                    ]
+                    # ensure it does not break if `ModelCheckpoint` args change
+                    if hasattr(checkpoint_callback, k)
+                },
+            }
+            if not self._checkpoint_name:
+                self._checkpoint_name = f"model-{self.experiment.id}"
+            artifact = wandb.Artifact(name=self._checkpoint_name, type="model", metadata=metadata)
+            artifact.add_file(p, name="model.ckpt")
+            aliases = ["latest", "best"] if p == checkpoint_callback.best_model_path else ["latest"]
+            self.experiment.log_artifact(artifact, aliases=aliases)
+
+            # The added line to wait for the artifact upload (before artifacts are accessed)
+            artifact.wait()
+
+            # remember logged models - timestamp needed in case filename didn't change (lastkckpt or custom name)
+            self._logged_model_time[p] = t
