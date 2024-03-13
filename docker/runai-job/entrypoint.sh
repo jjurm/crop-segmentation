cafadd116495ad0429b26b2131ad8a919d2db03f
Juraj Mičko <micko.juraj64@gmail.com>
2024-03-13 22:23:12 +0100

    docker entrypoint: load ENV_FILE if provided

--- docker/runai-job/entrypoint.sh
+++ docker/runai-job/entrypoint.sh
@@ -3,6 +3,11 @@ set -Eeo pipefail
 if [ -f $HOME/.profile ]; then
   . $HOME/.profile
 fi
+if [ -f $ENV_FILE ]; then
+  set -o allexport
+  . $ENV_FILE
+  set +o allexport
+fi
 # Print NODE_NAME if set
 if [ -n "$NODE_NAME" ]; then
   echo "NODE_NAME: $NODE_NAME"
