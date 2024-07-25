fe133ea1418dfc7e9820d32d92b52f7e3857af25
Juraj Mičko <micko.juraj64@gmail.com>
2024-07-26 01:38:44 +0200

    wandb_jobs: update active sampling experiment configs

--- wandb_jobs/experiments/active_sampling/generate_experiments.py
+++ wandb_jobs/experiments/active_sampling/generate_experiments.py
@@ -14,23 +14,23 @@ experiment_overrides = {
     "unet_bl2_loss": {
         "block_size": 2,
         "active_sampling_relevancy_score": "loss",
-        "learning_rate": 0.003,
+        "learning_rate": 0.001,
     },
     "unet_bl2_random": {
         "block_size": 2,
         "active_sampling_relevancy_score": "random",
-        "learning_rate": 0.01,
+        "learning_rate": [0.03, 0.01, 0.003],
     },
     "unet_bl2_rho": {
         "block_size": 2,
         "active_sampling_relevancy_score": "rho-loss-model-brisk-lion-807:best",
-        "learning_rate": 0.0003,
+        "learning_rate": [0.01, 0.003, 0.001, 0.0003],
     },
 
     "unet_bl8_ccm": {
         "block_size": 8,
         "active_sampling_relevancy_score": "uncertainty-margin",
-        "learning_rate": 0.001,
+        "learning_rate": 0.0003,
     },
     "unet_bl8_loss": {
         "block_size": 8,
@@ -40,12 +40,12 @@ experiment_overrides = {
     "unet_bl8_random": {
         "block_size": 8,
         "active_sampling_relevancy_score": "random",
-        "learning_rate": 0.01,
+        "learning_rate": [0.03, 0.01, 0.003],
     },
     "unet_bl8_rho": {
         "block_size": 8,
         "active_sampling_relevancy_score": "rho-loss-model-brisk-lion-807:best",
-        "learning_rate": 0.0001,
+        "learning_rate": [0.003, 0.001, 0.0003],
     },
 }
 
@@ -54,7 +54,7 @@ for experiment_name, experiment in experiment_overrides.items():
     template_copy["name"] = template_copy["name"].replace("$", experiment_name.replace("_", "-"))
     for key, value in experiment.items():
         template_copy["parameters"][key] = {
-            "value": value,
+            ("values" if isinstance(value, list) else "value"): value,
         }
     with open(f"{experiment_name}.yaml", "w") as f:
         yaml.dump(template_copy, f)
